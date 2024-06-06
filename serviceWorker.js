99bdc5d2323cc408ae22c1668e7300c49b120542
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-06-06 09:06:33 -0600

    Update V2

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,31 +1,32 @@
-const staticPage = "dev-user-site-v1";
-
+const staticPage = "dev-user-site-v1"
 const assets = [
     "/",
     "/index.html",
     "/styles.css",
     "/script.js",
 ]
-self.addEventListener("install",(installEvent) => {
+
+self.addEventListener("install", (installEvent) => {
     installEvent.waitUntil(
         caches.open(staticPage).then((cache) => {
-            cache.addAll(assets);
+            cache.addAll(assets)
         })
-    );
+    )
+
 })
 
-self.addEventListener("fetch",(fetchEvent) =>{
+self.addEventListener("fetch", (fetchEvent) => {
     fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then((response)=>{
-            return response || fetch(fetchEvent.request);
-        })
-    );
-});
+        caches.match(fetchEvent.request).then(response => response || fetch(fetchEvent.request)
+        )
+    )
+})
 
-if("serviceWorker" in navigator){
-    window.addEventListener("load",() =>{
-        navigator.serviceWorker.register("/serviceWorker.js")
-        .then((res)=>console.log("SeviceWorker registrado"))
-        .catch((err)=>console.log("ServiceWorker no registrado"))
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/serviceWorker.js").then(response => console.log("serviceWorker registrado")).catch(err => console.log("serviceWorker no registrado"))
     })
 }
+// .then() para manejar promesas, async awayt o callbacks
+
+// Como recuperar una peticion: con un fetch (  )
