0b99945dc4c5ab3117fa923894a57f55d635e36d
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-06-06 08:53:56 -0600

    ServiceWorker

---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const staticPage = "dev-user-site-v1";
+
+const assets = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/script.js",
+]
+self.addEventListener("install",(installEvent) => {
+    installEvent.waitUntil(
+        caches.open(staticPage).then((cache) => {
+            cache.addAll(assets);
+        })
+    );
+})
+
+self.addEventListener("fetch",(fetchEvent) =>{
+    fetchEvent.responseWith(
+        caches.match(fetchEvent.request).then((response)=>{
+            return response || fetch(fetchEvent.request);
+        })
+    );
+});
+
+if("serviceWorker" in navigator){
+    window.addEventListener("load",() =>{
+        navigator.serviceWorker.register("/serviceWorker.js")
+        .then((res)=>console.log("SeviceWorker registrado"))
+        .catch((err)=>console.log("ServiceWorker no registrado"))
+    })
+}
